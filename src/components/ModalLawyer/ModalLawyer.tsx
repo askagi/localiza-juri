000e784d9423b004e30b5c2838074a24f79ff993
José Costa <josecostasantos.jr@gmail.com>
2024-05-11 11:08:32 -0300

    Feat: modal lawyer

---
+++ src/components/ModalLawyer/ModalLawyer.tsx
@@ -0,0 +1,64 @@
+import { Button, Col, Modal, Row, Stack } from "react-bootstrap";
+import { BsWhatsapp } from "react-icons/bs";
+import { GoLaw } from "react-icons/go";
+import { User } from "../../types/User.type";
+
+type ModalLawyerProps = {
+  user: User;
+  show: boolean;
+  handleClose: () => void;
+};
+
+export function ModalLawyer({ user, show, handleClose }: ModalLawyerProps) {
+  console.log(user);
+
+  return (
+    <Modal show={show} onHide={handleClose} centered>
+      <Modal.Header closeButton>
+        <Modal.Title className="d-flex gap-2">
+          <GoLaw size={32} />
+          Informações
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Row>
+          <Col sm={4}>
+            <img
+              src={user.image}
+              alt=""
+              width={180}
+              height={180}
+              className="img-fluid rounded"
+            />
+          </Col>
+          <Col>
+            <h4>{user.name}</h4>
+            <hr />
+            <h6>Endereço:</h6>
+            <p className="text-muted">
+              Rua Mal. Deodoro, 118 - Centro, Alagoinhas - BA, 48005-020
+            </p>
+            <hr />
+            <h6 className="mb-3">Contato:</h6>
+            <Stack direction="horizontal">
+              <a
+                href="https://api.whatsapp.com/send?phone=12345678909876"
+                className="d-flex gap-2 align-items-center btn btn-success"
+                target="_blank"
+              >
+                <BsWhatsapp />
+                Enviar mensagem
+              </a>
+            </Stack>
+            <Button variant="link">adv@email.com</Button>
+          </Col>
+        </Row>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Fechar
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
